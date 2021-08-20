0e1b9f4f820e96482ba6c32b71a7743c1fe58e6d
Diogo <diogommr@gmail.com>
2021-08-20 23:08:55 +0100

    Updated Semana Maker program

--- pages/semanamaker.js
+++ pages/semanamaker.js
@@ -146,6 +146,7 @@ const SemanaMakerPage = {
                     <p class="event-text">Inscrições limitadas (5 vagas).</p>
                     <p class="event-text">Público-alvo: Público em geral a partir dos 9 anos.</p>
                     <p class="event-text">Duração: 2h</p>
+                    <p class="event-text">Requisitos: <strong>Deverás trazer computador portátil.</strong></p>
                     <a href="https://forms.gle/8aHKiTJ7ivU4nAb96" target="_blank" class="section-cta">Inscreve-te Já!</a>
                   </div>
 
